a2ff84c35625ba2c2b38f72a69cadcd5a6b26bac
amadorccasani <amadorccasaniarche@gmail.com>
2020-11-07 18:11:17 -0500

    s

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const {WebhookClient} = require('dialogflow-fulfillment');
-const api='http://127.0.0.1:8000/';
+// const api='http://127.0.0.1:8000/';
+const api = 'http://3.128.192.110/';
 const axios = require('axios');
 
 module.exports = {
@@ -7,4 +8,4 @@ module.exports = {
     api:api,
     axios:axios
    
-};
+};g
